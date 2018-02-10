772f70ca0c28157148eb91a94f56e9c17f6b95a0
Luke Stewart <lukestewart@gmail.com>
2018-02-09 23:31:23 -0700

    changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,19 +3,8 @@ module.exports = function(app) {
 
 // provides JSON
 app.get("/api/friends", function(req, res) {
-    var chosen = req.params.friends;
-  
-    if (chosen) {
-      console.log(chosen);
-  
-      for (var i = 0; i < friends.length; i++) {
-        if (chosen === friends[i].routeName) {
-          return res.json(friends[i]);
-        }
-      }
-      return res.json(false);
-    }
-    return res.json(friends);
+   // testing the .get
+    res.send('test');
   });
   
   // Create New entry - takes in JSON input
@@ -23,13 +12,8 @@ app.get("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     var newFriend = req.body;
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
   
     console.log(newFriend);
-  
-    characters.push(newFriend);
   
     res.json(newFriend);
   });
